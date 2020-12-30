e560659c1310dbeb12b7105740998001fbd13ccd
Clark Taylor <clark.n.taylor@gmail.com>
2020-12-30 00:26:33 -0500

    Switched to tkinter

--- BlobGui.py
+++ BlobGui.py
@@ -1,16 +1,11 @@
-import kivy
 import cv2
 import threading
 from queue import Queue
 
-from kivy.app import App
-
-from kivy.uix.button import Button
-from kivy.uix.texture import Texture
-
-class BlobGui(App):
-    def build(self):
-        return Button(text="Blob Threshold Tuning")
+from PIL import Image
+from PIL import ImageTk
+import tkinter as tk
+import sys
 
 class CaptureThread:
     def __init__(self, out_queue, vid_num=0):
@@ -40,8 +35,31 @@ class CaptureThread:
 
 
 if __name__ == "__main__":
+    vc = cv2.VideoCapture(0)
+    if not vc.isOpened():
+        print("Can't open the video stream.  Quitting!")
+        sys.exit()
+    ret,frame = vc.read()
+    if not ret:
+        print("Can't get an image from the video stream. Quitting!")
+        sys.exit()
+
+    window = tk.Tk()
+
+    #Take the image and turn it into something tkinter can use
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    tmp_im = Image.fromarray(image)
+    disp_im = ImageTk.PhotoImage(tmp_im)
+
+    im_spot = tk.Label(image=disp_im)
+    im_spot.image = disp_im
+    im_spot.grid(column=0, row=0)
+    window.mainloop()
+
+    '''
     #First, setup the cv2 capture thread
     captured_queue= Queue()
     ct = CaptureThread(captured_queue)
     capture_thread = threading.Thread(target=ct.runit)
     BlobGui().run()
+    '''
